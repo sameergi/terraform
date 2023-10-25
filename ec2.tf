f7ba95d1738aff6b221dcbeab2441e0681a15beb
sameergi <139549974+sameergi@users.noreply.github.com>
2023-10-25 18:54:46 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 #creating 1st ec2 instance in public subnet
 resource "aws_instance" "instance1" {
-  ami                         = "ami-03d294e37a4820c21"
+  ami                         = "ami-06006e8b065b5bd46"
   instance_type               = "t2.micro"
   key_name                    = "mumbaikey"
   vpc_security_group_ids      = ["${aws_security_group.demosgid.id}"]
