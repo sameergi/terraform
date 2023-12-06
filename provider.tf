4a30d4779f3f9eeec625aefabecf912a51cdd4e0
Raghuveersingh1436 <145828543+Raghuveersingh1436@users.noreply.github.com>
2023-12-06 11:05:57 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = ""
-  secret_key = ""
+  access_key = "AKIAUNLCNKLIUEQ4W7EC"
+  secret_key = "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"
   region     = "ap-south-1"
 }
 
